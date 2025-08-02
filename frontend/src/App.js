352f042d63d8160b714d0fad405e146aa4aa4965
김혜림 <redacted@users.noreply.github.com>
2025-08-02 12:29:21 +0900

    feat: 화면 구현 및 api 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
+import MailPage from "./pages/MailPage";
 import ChatPage from "./pages/ChatPage";
-
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/" element={<ChatPage />} />
+      <Route path="/mail" element={<MailPage />} />
     </Routes>
   );
 }
-
---
+++ frontend/src/axios/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const VITE_SERVER_DOMAIN = process.env.REACT_APP_SERVER_DOMAIN;;
+
+export const jsonAxios = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_DOMAIN,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
